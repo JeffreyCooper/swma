5a6ef3a4be1b3728e614510a26427374b3eaa8e7
Jeffrey Cooper <jcooper37@cnm.edu>
2017-07-08 11:20:44 -0600

    Add documentation page

---
+++ epic/documentation.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--@author: Jeffrey Cooper <jcooper37@cnm.edu>
+	Date: 26-Apr-17 Time: 8:27 AM-->
+	<head>
+		<meta charset="utf-8" />
+		<title>PWP for Jeffrey Cooper - Milestone 1</title>
+		<!-- Custom CSS -->
+		<link rel="stylesheet" href="styles-documentation.css" type="text/css" />
+	</head>
+	<body>
+		<header>
+			<h1>PWP for Jeffrey Cooper</h1>
+			<h2>Milestone 1</h2>
+		</header>
+		<main>
+			<h3>Purpose</h3>
+			<!--Write at least one sentence to address each of the following using valid HTML markup.-->
+			<p>I need a website that is flexible enough to use for all of the following scenarios:</p>
+			<ol>
+				<li>Resume: Send prospective employers to this site so they can learn about my coding experience with sample websites, my education, my previous experience in other industries and my life interests.</li>
+				<li>Clients: Send prospective clients to this site to discover my capabilities, previous clients and referrals, as well as my personal interests.</li>
+				<li>Network: Send new contacts with which I hope to build a networking relationship so they can learn about my history, my interests, my current work related activities and my contact information.</li>
+			</ol>
+			<!--2. Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?-->
+			<h3>Audience</h3>
+			<p>My audience will be prospective clients, potential employers, peers in web development, peers in previous industries.</p>
+
+			<!--3. Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
+-To acquire full or part-time employment or an internship.
+-To increase sales and/or conversion rates for an existing business.
+-To attract freelance clients.
+-To serve as a gallery to showcase and promote your work.
+-To increase the reach of a new or established social media presence (as measured via analytics tools).-->
+
+			<h3>Goals</h3>
+			<ol>
+				<li>To acquire employment.</li>
+				<li>To attract freelance clients.</li>
+				<li>To promote myself for potential part-time jobs in the music industry.</li>
+				<li>To serve as a repository for past projects, performances and classes/workshops I've presented.</li>
+				<li>To advertise current business that I am involved in.</li>
+			</ol>
+			<h3>PERSONA</h3>
+			<!--Define one persona that represents the primary target audience for your Personal Website. More information on how to create a persona for your project here: http://www.webcredible.com/blog-reports/web-usability/personas.shtml
+
+Your persona should feature the following at minimum:
+
+Name
+- A detailed description that includes demographic information such as: age, job or role, relevant technology being used, behaviors and attitudes, and the situation or scenario in which they will use your site.
+- Goal(s) regarding the use of your site.
+
+Additional information regarding your persona may also include:
+
+- Frustrations your persona may be facing.
+- Additional information your persona may need in order to achieve their goal.-->
+
+
+			<h3>Jerry Foster</h3>
+			<ol>
+				<li>Age: 59</li>
+				<li>Job: Director of Music Ministries</li>
+				<li>Technologies: Windows 10, iPhone</li>
+				<li>Attitude: Luddite with technology.</li>
+				<li>Behavior: Desire to integrate her groups with the internet</li>
+				<li>Goals: Learn about my skills as a web developer. See some sample sites. Consider hiring me to build a new website for her choirs.</li>
+				<li>Potential: Well connected in the Albuquerque Choral Music scene. Potential for referrals is high. She can easy send other potential clients to this site.</li>
+			</ol>
+
+			<h3>USE CASE</h3>
+
+			<!--A use case should be based on your persona’s motivations and goals. Your use case should detail the time, location, technology used, and the purpose or goal of your Persona's use of your site.-->
+
+			<p>Jerry knows about me already and likes my services. She is speaking with a potential referral client at a city wide workshop for community choirs. She wants to introduce my services, so she pulls out her iPhone and attempts to show my site to the potential client, including sample web pages, musical background and contact information.</p>
+		</main>
+	</body>
+</html>
