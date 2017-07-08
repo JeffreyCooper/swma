91f956431fb635a2792844c6836e081d2d64ca24
Jeffrey Cooper <jcooper37@cnm.edu>
2017-07-08 14:58:49 -0600

    Add content to epic documentation, including Purpose, Goals, Prospective audience, Use Cases

--- epic/documentation.php
+++ epic/documentation.php
@@ -4,27 +4,48 @@
 	Date: 26-Apr-17 Time: 8:27 AM-->
 	<head>
 		<meta charset="utf-8" />
-		<title>PWP for Jeffrey Cooper - Milestone 1</title>
+		<title>SWMA Website 2.0</title>
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="styles-documentation.css" type="text/css" />
 	</head>
 	<body>
 		<header>
-			<h1>PWP for Jeffrey Cooper</h1>
-			<h2>Milestone 1</h2>
+			<h1>SWMA Website 2.0</h1>
+			<h2>Purpose, Goals, Persona, Use Case</h2>
 		</header>
 		<main>
 			<h3>Purpose</h3>
 			<!--Write at least one sentence to address each of the following using valid HTML markup.-->
-			<p>I need a website that is flexible enough to use for all of the following scenarios:</p>
+			<p>The new SWMA website will focus on the following areas:</p>
 			<ol>
-				<li>Resume: Send prospective employers to this site so they can learn about my coding experience with sample websites, my education, my previous experience in other industries and my life interests.</li>
-				<li>Clients: Send prospective clients to this site to discover my capabilities, previous clients and referrals, as well as my personal interests.</li>
-				<li>Network: Send new contacts with which I hope to build a networking relationship so they can learn about my history, my interests, my current work related activities and my contact information.</li>
+				<li>Catalog: Provide a complete catalog of the SWMA artwork. It must be searchable. It must contain high-quality images of the artwork. It must contain descriptions of each piece of art. It must represent the various Series and the art representing each.</li>
+				<li>Customers: The website should present to customers the work of SWMA. It must include an option for purchases via mail online.</li>
+				<li>Wholesalers: The website should present wholesalers with bulk ordering information.</li>
+				<li>Contact information: There will be a contact form with contact details to reach SWMA.</li>
 			</ol>
+			<h5>Additional Website Features</h5>
+			<p>Accounts: Login - Each active customer can login to the website.</p>
+			<ol>
+				<li>Customer information retained through a detailed login procedure.</li>
+				<li>Each customer must be distinguished as either an INDIVIDUAL or a COMPANY. The former will be considered retail while the later will be considered Wholesale.</li>
+				<li>A COMPANY must inlude their state issued CRS number to be kept on record, which will entitle them to being charged without sales tax in NM.</li>
+				<li>A COMPANY that is NOT in NM will be afforded Wholesale status by including their TIN number. By doing so, they will not be charged Sales Tax.</li>
+				<li>An INDIVIDUAL who is NOT a resident of NM will not be charged tax. However, they should be informed that they could be subject to tax in the future if the state decides to change the law, as they tried in 2017.</li>
+				<li>Each entity creating an account must agree to be contacted by SWMA for any company or account related news.</li>
+				<li>An opt-in feature will allow the creation of a mailing list of INDIVIDUALS.</li>
+				<li>All COMPANY accounts will be added to a mailing list for Wholesalers. This is automatic, without an opt-in feature. There must be a disclosure of this list during sign-up.</li>
+			</ol>
+			<h5>A Phase 2 feature will be accounts to make purcheses online and track accounts.</h5>
 			<!--2. Audience: Who will be using or viewing your site? Who is this site designed for, and who is your audience comprised of? Could it be for your professional peers and other developers, potential employers, customers, or clients, etc.?-->
 			<h3>Audience</h3>
-			<p>My audience will be prospective clients, potential employers, peers in web development, peers in previous industries.</p>
+			<p>The Audience for the SWMA website has two groups.</p>
+			<p>Group 1 - Wholesalers: This is companies, like gift shops, that will carry the SWMA product.</p>
+			<p>Group 2 - Individuals: These are people who have:</p>
+			<ul>
+				<li>An interest in the artwork from SWMA.</li>
+				<li>Have purchased artwork from SWMA and have the potential to purchase more.</li>
+				<li>Have been gifted artwork from SWMA and want more information.</li>
+			</ul>
 
 			<!--3. Goal: This is should be a quantifiable goal if possible. For example, a goal for your PWP could be:
 -To acquire full or part-time employment or an internship.
@@ -35,11 +56,11 @@
 
 			<h3>Goals</h3>
 			<ol>
-				<li>To acquire employment.</li>
-				<li>To attract freelance clients.</li>
-				<li>To promote myself for potential part-time jobs in the music industry.</li>
-				<li>To serve as a repository for past projects, performances and classes/workshops I've presented.</li>
-				<li>To advertise current business that I am involved in.</li>
+				<li>To advertise the collected works of SWMA.</li>
+				<li>To communmicate the Mission, Purpose and History of SWMA.</li>
+				<li>To communicate current promotions, sales, locations of Fairs scheduled, etc.</li>
+				<li>To communicate information on each individual piece of art by SWMA.</li>
+				<li>To provide a means to contact SWMA.</li>
 			</ol>
 			<h3>PERSONA</h3>
 			<!--Define one persona that represents the primary target audience for your Personal Website. More information on how to create a persona for your project here: http://www.webcredible.com/blog-reports/web-usability/personas.shtml
@@ -56,22 +77,34 @@ Additional information regarding your persona may also include:
 - Additional information your persona may need in order to achieve their goal.-->
 
 
-			<h3>Jerry Foster</h3>
+			<h3>1. Jerry Foster</h3>
 			<ol>
 				<li>Age: 59</li>
 				<li>Job: Director of Music Ministries</li>
 				<li>Technologies: Windows 10, iPhone</li>
 				<li>Attitude: Luddite with technology.</li>
-				<li>Behavior: Desire to integrate her groups with the internet</li>
-				<li>Goals: Learn about my skills as a web developer. See some sample sites. Consider hiring me to build a new website for her choirs.</li>
-				<li>Potential: Well connected in the Albuquerque Choral Music scene. Potential for referrals is high. She can easy send other potential clients to this site.</li>
+				<li>Behavior: Interest in SWMA product, artwork, paintings and future Fairs.</li>
+				<li>Goals: Learn about the history of SWMA. Learn about the artwork available from SWMA. Learn how to contact SWMA.</li>
+				<li>Potential: Interest in purchasing from SWMA. Interest in attending a FAIR where SWMA is presenting. Word-of-mouth promotion of SWMA.</li>
 			</ol>
 
 			<h3>USE CASE</h3>
 
 			<!--A use case should be based on your persona’s motivations and goals. Your use case should detail the time, location, technology used, and the purpose or goal of your Persona's use of your site.-->
 
-			<p>Jerry knows about me already and likes my services. She is speaking with a potential referral client at a city wide workshop for community choirs. She wants to introduce my services, so she pulls out her iPhone and attempts to show my site to the potential client, including sample web pages, musical background and contact information.</p>
+			<p>Jerry heard about SWMA from a friend who purchased a miniature painting. She was told that she found it at an Art Fair. Jerry wants to see more. She loves artwork, especially from the Southwest. She wants to know: Who we are? What we sell? How much it costs? Where can she find it? How can she easily contact us?</p>
+
+			<h3>2. Gifts Galore</h3>
+			<ol>
+				<li>Store is 5 years old.</li>
+				<li>Owner is Megan Truly.</li>
+				<li>Buyer is Scott Aarons.</li>
+				<li>Market is gifts, mainly Christian based and features local producers.</li>
+				<li>Tech: Store computer is an HP laptop with Win10. Owner has iPhone. Buyer has an android.</li>
+				<li>Attitude: Owner is a luddite. Buyer is proficient on computers. Buyer runs the company website.</li>
+				<li>Goals: After an initial in-person meeting, owner asks buyer to check out SWMA and provides him with the web address. He will look for History, Catalogue, Prices, Info on each piece, other places that carry the product, contact information.</li>
+				<li>Potential: Interest in carrying SWMA on their shelves. Wants some sort of exclusive deal. Owner is sold but buyer is a skeptic.</li>
+			</ol>
 		</main>
 	</body>
 </html>
